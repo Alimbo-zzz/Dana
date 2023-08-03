a997b9ba98834e6fe6a4f48f833da58cfeb68ddd
Alim Mambetov <alimbo333@gmail.com>
2023-08-04 00:13:11 +0300

    Updates

---
+++ index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Landing } from '@/pages';
+
+// modules
+import '@/styles/index.scss';
+
+
+// vars
+const root = document.getElementById('root');
+
+ReactDOM.createRoot(root).render(<>
+	<Landing />
+</>)
